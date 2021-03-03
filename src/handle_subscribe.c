b89e4f45be960ba1982877c04e6af221c5fbf1d2
jasonyux <jasonyux16@gmail.com>
2021-03-03 16:48:07 +0800

    Amended Subscription Flooding

--- src/handle_subscribe.c
+++ src/handle_subscribe.c
@@ -28,7 +28,68 @@ Contributors:
 #include "property_mosq.h"
 #include "send_mosq.h"
 
+/* code added by Xiao */
+char *strip_prefix(char *text, char* prefix)
+{
+	char *tmp = text;
+	char *tmp_prefix = prefix;
+	/* strip the local prefix for the db generated name*/
+	size_t strip_count = strlen(prefix);
+	while(*text && *tmp_prefix && *text++ == *tmp_prefix++)
+		strip_count--;
+	/* resets if not matching */
+	if(strip_count)
+		text = tmp;
+	return text;
+}
 
+int recursive_sub(char *from_id, char *to_id)
+{
+	char *prefix = "ubuntu.", *tok;
+	char *mapping_symbol = "->";
+
+	char *from_id_cpy = malloc(strlen(from_id)+1);
+	char *to_id_cpy = malloc(strlen(to_id)+1);
+	strcpy(from_id_cpy, from_id);
+	strcpy(to_id_cpy, to_id);
+
+	char *from_id_cpy_tmp = strip_prefix(from_id_cpy, prefix);
+	char *to_id_cpy_tmp = strip_prefix(to_id_cpy, prefix);
+
+	char *from_f_t[2];
+	char *to_f_t[2];
+
+	int i = 0;
+	tok = strtok(from_id_cpy_tmp, mapping_symbol);
+	while(tok && i<2){
+		from_f_t[i++] = tok;
+		tok = strtok(NULL, mapping_symbol);
+	}
+	
+	int j = 0;
+	tok = strtok(to_id_cpy_tmp, mapping_symbol);
+	while(tok && j<2){
+		to_f_t[j++] = tok;
+		tok = strtok(NULL, mapping_symbol);
+	}
+
+	/* recursive if b1->b7 and b7->b1 */
+	int result = 0;
+	if(i==2 && j==2){
+		printf("comparing %s->%s and %s->%s\n", from_f_t[0], from_f_t[1], to_f_t[0], to_f_t[1]);
+		if((strcmp(from_f_t[0], to_f_t[1]) == 0) && (strcmp(from_f_t[1], to_f_t[0]) == 0)){
+			result = 1;
+		}
+		printf("same (1=true)? %d\n", result);
+	}
+	
+	free(from_id_cpy);
+	free(to_id_cpy);
+
+	return result;
+}
+
+/* end of code added by Xiao */
 
 int handle__subscribe(struct mosquitto *context)
 {
@@ -183,9 +244,32 @@ int handle__subscribe(struct mosquitto *context)
 					for (int j = 0; j < old_topics_num; j++){
 						char *old_topic = old_topics[j].remote_topic;
 						if (strlen(old_topic) == strlen(sub) && strncmp(old_topic, sub, strlen(sub)))
-							same_topic++;
+							same_topic = 1;
 					}
-					if (!same_topic){
+					char *fromSender = context->id;
+					char *toSender = db.bridges[i]->bridge->name;
+					char *local_prefix = "local.";
+					/* strip the local prefix for the db generated name*/
+					toSender = strip_prefix(toSender, local_prefix);
+					
+					///* on error, just continue and not send it*/
+					//if(snprintf(toSender, sizeof(toSender), "ubuntu.bridge-%d", db.config->listeners->port) <= 0)
+					//	continue;
+
+					/* send if:
+						1. the topic is not redundant (compared to list of topics already subscribed)
+						2. not sending the subscription back to the broker, who actually sent it to me
+					 */
+					/* char *expectedTemplate = "ubuntu.bridge-dddd";
+					int cmpSize = strlen(expectedTemplate) < strlen(fromSender) ? strlen(expectedTemplate): strlen(fromSender);
+					if (!same_topic && strncmp(fromSender, toSender, (size_t) cmpSize)){
+						char *new_topics[new_topics_num];
+						new_topics[0] = sub;
+						send__subscribe(db.bridges[i], NULL, 1, new_topics, qos, NULL);
+					} */
+
+					if (!same_topic && !recursive_sub(toSender, fromSender)){
+						printf("my subflooding send\n");
 						char *new_topics[new_topics_num];
 						new_topics[0] = sub;
 						send__subscribe(db.bridges[i], NULL, 1, new_topics, qos, NULL);
@@ -194,7 +278,7 @@ int handle__subscribe(struct mosquitto *context)
 				}
 			}
 
-			/* start of subscription flooding code */
+			/* end of subscription flooding code */
 
 			allowed = true;
 			rc2 = mosquitto_acl_check(context, sub, 0, NULL, qos, false, MOSQ_ACL_SUBSCRIBE);
